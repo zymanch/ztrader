eb1153aab1ce63ae95f547d1d3a2a5ffa3937e48
zymanch <zymanch@gmail.com>
2021-04-11 17:02:02 +0300

    support more currencies

--- src/migrations/m210404_052345_detector.php
+++ src/migrations/m210404_052345_detector.php
@@ -22,7 +22,48 @@ class m210404_052345_detector extends Migration
             )
             COLLATE='utf8mb4_unicode_ci'
             ENGINE=InnoDB");
-            $this->insert('currency',['currency_id'=>1,'code'=>'BTC','name'=>'BitCoin','position'=>1]);
+
+            $this->execute("INSERT INTO `currency` VALUES (1, 'BTC', 'BitCoin', 1)");
+            $this->execute("INSERT INTO `currency` VALUES (2, 'XRP', 'Ripple', 2)");
+            $this->execute("INSERT INTO `currency` VALUES (3, 'ETH', 'Ethereum', 3)");
+            $this->execute("INSERT INTO `currency` VALUES (4, 'BNB', 'Binance Coin', 4)");
+            $this->execute("INSERT INTO `currency` VALUES (5, 'XLM', 'Stellar Lumens', 5)");
+            $this->execute("INSERT INTO `currency` VALUES (6, 'LTC', 'Litecoin', 6)");
+            $this->execute("INSERT INTO `currency` VALUES (7, 'DOG', 'DogeCoin', 7)");
+            $this->execute("INSERT INTO `currency` VALUES (8, 'GRT', 'The Graph', 8)");
+            $this->execute("INSERT INTO `currency` VALUES (9, 'ALG', 'Algorand', 9)");
+            $this->execute("INSERT INTO `currency` VALUES (10, 'COM', 'Compound', 10)");
+            $this->execute("INSERT INTO `currency` VALUES (11, 'SNX', 'Synthetix Network Token', 11)");
+            $this->execute("INSERT INTO `currency` VALUES (12, 'YFI', 'yearn.finance', 12)");
+            $this->execute("INSERT INTO `currency` VALUES (13, 'CAK', 'PancakeSwap', 13)");
+            $this->execute("INSERT INTO `currency` VALUES (14, 'GRT', 'GrtDown', 14)");
+            $this->execute("INSERT INTO `currency` VALUES (15, 'MKR', 'Maker', 15)");
+            $this->execute("INSERT INTO `currency` VALUES (16, 'TOM', 'TomoChain', 16)");
+            $this->execute("INSERT INTO `currency` VALUES (17, 'BUS', 'BUSD', 17)");
+            $this->execute("INSERT INTO `currency` VALUES (18, 'BCH', 'Bitcoin Cash', 18)");
+            $this->execute("INSERT INTO `currency` VALUES (19, 'ADA', 'Cardano', 19)");
+            $this->execute("INSERT INTO `currency` VALUES (20, 'BAT', 'Basic Attention Token', 20)");
+            $this->execute("INSERT INTO `currency` VALUES (21, 'MAT', 'Polygon', 21)");
+            $this->execute("INSERT INTO `currency` VALUES (22, 'VET', 'VeChain', 22)");
+            $this->execute("INSERT INTO `currency` VALUES (23, 'HBA', 'Hedera Hashgraph', 23)");
+            $this->execute("INSERT INTO `currency` VALUES (24, 'ATO', 'Cosmos', 24)");
+            $this->execute("INSERT INTO `currency` VALUES (25, 'CHZ', 'Chiliz', 25)");
+            $this->execute("INSERT INTO `currency` VALUES (26, 'LIN', 'ChainLink', 26)");
+            $this->execute("INSERT INTO `currency` VALUES (27, 'QTU', 'Qtum', 27)");
+            $this->execute("INSERT INTO `currency` VALUES (28, 'DOT', 'Polkadot', 28)");
+            $this->execute("INSERT INTO `currency` VALUES (29, 'DAI', 'Dai', 29)");
+            $this->execute("INSERT INTO `currency` VALUES (30, 'ZEC', 'Zcash', 30)");
+            $this->execute("INSERT INTO `currency` VALUES (31, 'ZIL', 'Ziliqa', 31)");
+            $this->execute("INSERT INTO `currency` VALUES (32, 'BTT', 'BitTorrent', 32)");
+            $this->execute("INSERT INTO `currency` VALUES (33, 'DAS', 'Dash', 33)");
+            $this->execute("INSERT INTO `currency` VALUES (34, 'EOS', 'EOS', 34)");
+            $this->execute("INSERT INTO `currency` VALUES (35, 'NAN', 'NANO', 35)");
+            $this->execute("INSERT INTO `currency` VALUES (36, 'PAX', 'Paxos Standart', 36)");
+            $this->execute("INSERT INTO `currency` VALUES (37, 'TRX', 'TRON', 37)");
+            $this->execute("INSERT INTO `currency` VALUES (38, 'USD', 'TetherUS', 38)");
+            $this->execute("INSERT INTO `currency` VALUES (39, 'XTZ', 'Tezos', 39)");
+            $this->execute("INSERT INTO `currency` VALUES (40, 'ETC', 'Ethereum Classic', 40)");
+
         }
         if ($this->db->getTableSchema('seller', true) === null) {
             $this->execute("CREATE TABLE `seller` (
---
+++ userscript.js
@@ -0,0 +1,51 @@
+var $from = $("#issuer-profile-export-from");
+var $fromD = $("#issuer-profile-export-from-d");
+var $fromM = $("#issuer-profile-export-from-m");
+var $fromY = $("#issuer-profile-export-from-y");
+
+var $to = $("#issuer-profile-export-to");
+var $toD = $("#issuer-profile-export-to-d");
+var $toM = $("#issuer-profile-export-to-m");
+var $toY = $("#issuer-profile-export-to-y");
+
+var $block = $("#issuer-profile-export-button");
+var $button = $("<input type='button' value='СОбрать'>");
+var $form = $("#chartform");
+var $fileName = $("#issuer-profile-export-file-name");
+var blank = $("#issuer-profile-export-contract").val();
+
+var from = null;
+var maxDate = null;
+
+
+$block.append($button);
+var tick = function () {
+    $from.val((from.getDate()<10?'0':'')+from.getDate()+'.'+(from.getMonth()<9?'0':'')+(from.getMonth()+1)+'.'+from.getFullYear());
+    $fromD.val(from.getDate());
+    $fromM.val(from.getMonth());
+    $fromY.val(from.getFullYear());
+
+    var to =  new Date(from.getTime()+3600*24*1000);
+    $to.val((to.getDate()<10?'0':'')+to.getDate()+'.'+(to.getMonth()<9?'0':'')+(to.getMonth()+1)+'.'+to.getFullYear());
+    $toD.val(to.getDate());
+    $toM.val(to.getMonth());
+    $toY.val(to.getFullYear());
+    console.log(from+' to '+to);
+
+    $fileName.val(blank+'_'+from.getYear()+(from.getMonth()<9?'0':'')+(from.getMonth()+1)+(from.getDate()<10?'0':'')+from.getDate()+
+        '_'+to.getFullYear()+(to.getMonth()<9?'0':'')+(to.getMonth()+1)+(to.getDate()<10?'0':'')+to.getDate());
+
+    $form.submit();
+    if (from.getTime()<maxDate.getTime()){
+        from.setTime(from.getTime()+3600*24*1000);
+        setTimeout(tick, 10000);
+    }
+}
+$button.click(function(e) {
+    e.stopPropagation();
+    from = new Date($from.val().substr(6,4)+'-'+$from.val().substr(3,2)+'-'+$from.val().substr(0,2));
+    maxDate = new Date($to.val().substr(6,4)+'-'+$to.val().substr(3,2)+'-'+$to.val().substr(0,2));
+    console.log(from+' to '+maxDate);
+    tick();
+    return false;
+});
